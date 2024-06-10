108edb32073d89eaf5dc871dd66f39553e71aeee
tyoo-18 <bukanbowo123@gmail.com>
2024-06-10 11:05:50 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body class="bg-secondary">
+    {{-- navbar --}}
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('product.index') }}">Produk</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    {{-- /navbar --}}
+
+    
+    <h1 style="color: white;">Web Perekap Toko nabila</h1> <br> <br>
+    
+    <h2 style="color: white;">Product yang telah di rekap</h2>
+    <table class="table table-hover">
+        <thead class="table-primary">
+            <tr style="color:cadetblue;">
+                <th>#</th>
+                <th>Tanggal</th>
+                <th>Nama Barang</th>
+                <th>Jumlah Barang</th>
+                <th>Harga Barang</th>
+            </tr>
+        </thead>
+        <tbody style="color: white;">
+            @if($products->count() > 0)
+                @foreach($products as $rs)
+                    <tr>
+                        <td class="align-middle">{{ $loop->iteration }}</td>
+                        <td class="align-middle">{{ $rs->tanggal }}</td>
+                        <td class="align-middle">{{ $rs->nama_barang }}</td>
+                        <td class="align-middle">{{ $rs->jumlah_barang }}</td>
+                        <td class="align-middle">{{ $rs->harga_barang }}</td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td class="text-center" colspan="5">Product not found</td>
+                </tr>
+            @endif
+        </tbody>
+    </table>
+
+   
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
+ 
+ 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+ 
+Route::resource('/product', ProductController::class);
+
